8949de31ca934ac9be9d7a4d9b336c50bdbcefac
kevinkiklee <kevin.kik.lee@gmail.com>
2018-08-19 13:27:02 -0400

    Setup socket.io skeleton on the client

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import io from 'socket.io-client';
+
 import './index.css';
 import App from './App';
+
+fetch('/api/chat/create')
+
+const socket = io.connect('http://localhost:3001/abc');
+
+socket.on('serverMessage', function (data) {
+  console.log(data);
+  socket.emit('clientMessage', { fromClient: 'hello' });
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
