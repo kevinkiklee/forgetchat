4171c8885200917d5aaf093c9a0ef10a784e39c7
kevinkiklee <kevin.kik.lee@gmail.com>
2018-08-19 13:44:42 -0400

    Remove localhost address for client socket connection

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import App from './App'
 fetch('/api/chat/create')
   .then(res => res.json())
   .then(({ chatId }) => {
-    const socket = io.connect(`http://localhost:3001/${chatId}`)
+    const socket = io.connect(`/${chatId}`)
 
     socket.on('serverMessage', function (data) {
       console.log(data)
