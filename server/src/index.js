4108bf286459028b9d5f39fca4c244164c73e245
kevinkiklee <kevin.kik.lee@gmail.com>
2018-08-19 13:27:12 -0400

    Setup socket.io skeleton on the server

--- server/src/index.js
+++ server/src/index.js
@@ -1,34 +1,34 @@
-const fs = require('fs')
 const path = require('path')
 
 const express = require('express')
+const io = require('socket.io')
+
+const app = express()
+const server = require('http').Server(app)
+const serverIo = io(server)
 
 const PORT = process.env.PORT || 3001
-const app = express()
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-
 const chats = {}
 
-app.use(express.static(path.join(__dirname, '../../client/build')));
+app.use(express.static(path.join(__dirname, '../../client/build')))
 
 app.get('/', (req, res) => {
   console.log(`GET / - SUCCESS - Index served`)
   res.sendFile('index.html')
 })
 
-app.get('/api/lock/:chatId', (req, res) => {
+app.get('/api/chat/lock/:chatId', (req, res) => {
   const chatId = req.params.chatId
 
   if (chats[chatId]) {
     chats[chatId].locked = true
-    console.log(`GET /api/lock/:chatId - SUCCESS - ${chatId} locked`)
+    console.log(`GET /api/chat/lock/:chatId - SUCCESS - ${chatId} locked`)
   } else {
-    console.log(`GET /api/lock/:chatId - ERROR - ${chatId} does not exist`)
+    console.log(`GET /api/chat/lock/:chatId - ERROR - ${chatId} does not exist`)
   }
 })
 
-app.get('/api/create', (req, res) => {
+app.get('/api/chat/create', (req, res) => {
   const chatId = Math.random().toString(36).slice(2)
 
   chats[chatId] = {
@@ -37,30 +37,18 @@ app.get('/api/create', (req, res) => {
     locked: false,
   }
 
-  console.log(`GET /api/create - SUCCESS - ${chatId} created`)
-  res.send(`GET /api/create - SUCCESS - ${chatId} created`)
-})
+  const chatIo = serverIo.of('/abc');
 
-app.get('/test', (req, res) => {
-  const chatId = req.params.chatId
+  chatIo.on('connection', socket => {
+    chatIo.emit('serverMessage', { fromServer: 'hello' })
 
-  if (chats[chatId]) {
-    res.send('okay')
-  } else {
-    console.log(`GET /:chatId - ERROR - ${chatId} does not exist`)
-    res.send(`GET /:chatId - ERROR - ${chatId} does not exist`)
-  }
-})
+    socket.on('clientMessage', data => {
+      console.log(data)
+    })
+  })
 
-app.get('/room/:chatId', (req, res) => {
-  const chatId = req.params.chatId
-
-  if (chats[chatId]) {
-    res.send('okay')
-  } else {
-    console.log(`GET /:chatId - ERROR - ${chatId} does not exist`)
-    res.send(`GET /:chatId - ERROR - ${chatId} does not exist`)
-  }
+  console.log(`GET /api/chat/create - SUCCESS - ${chatId} created`)
+  res.send(chatId)
 })
 
 server.listen(PORT, error => {
@@ -70,10 +58,3 @@ server.listen(PORT, error => {
 
   console.log(`Server running on port ${PORT}`)
 })
-
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
