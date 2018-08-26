4a9e048d3e80e5eb7793ca06048fb1b3b650c6b3
kevinkiklee <kevin.kik.lee@gmail.com>
2018-08-26 11:41:36 -0400

    Implement setupSocketClient helper

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import io from 'socket.io-client'
 
 import './app.css'
 import Chat from './Chat'
 
 const chatId = window.location.pathname.split('/c/')[1]
 
-try {
-  fetch(`/api/validate/${chatId}`)
-    .then(res => res.json())
-    .then(({ isValid }) => {
-      if (isValid) {
-        window.socketIo = io.connect(`/${chatId}`)
+const app = async () => {
+  try {
+    const response = await fetch(`/api/validate/${chatId}`)
 
-        ReactDOM.render(<Chat />, document.getElementById('root'))
-      }
-    })
-} catch (error) {
-  console.error(error)
+    if (!response.ok) {
+      throw Error(response.statusText)
+    }
+
+    const { isValid } = await response.json()
+
+    if (isValid) {
+      ReactDOM.render(<Chat chatId={chatId} />, document.getElementById('root'))
+    }
+  } catch (error) {
+    throw Error(error)
+  }
 }
+
+app()
