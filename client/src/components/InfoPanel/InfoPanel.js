7273b63789c745cde34418cbbff5af827ab0470e
kevinkiklee <kevin.kik.lee@gmail.com>
2018-08-27 20:52:52 -0400

    Populate InfoPanel skeleton

--- client/src/components/InfoPanel/InfoPanel.js
+++ client/src/components/InfoPanel/InfoPanel.js
@@ -5,7 +5,32 @@ class InfoPanel extends Component {
   render() {
     return (
       <div className='info-panel-container'>
-        info-panel
+        <div className='info-panel-container--top'>
+          <div className='info-panel__app-info'>
+            <div className='info-panel__app-info__logo'>
+              logo
+            </div>
+            <div className='info-panel__app-info__name'>
+              name
+            </div>
+          </div>
+          <div className='info-panel__connection-status'>
+            securely connected
+          </div>
+          <div className='info-panel__participants'>
+            <div className='info-panel__participant'>
+              alpha
+            </div>
+            <div className='info-panel__participant'>
+              bravo
+            </div>
+          </div>
+        </div>
+        <div className='info-panel-container--bottom'>
+          <div className='info-panel__explanation'>
+            lorem ipsum
+          </div>
+        </div>
       </div>
     )
   }
