c4cfb6457f7fcefb141b9f3600abde03176196f9
kevinkiklee <kevin.kik.lee@gmail.com>
2018-08-31 15:08:33 -0400

    Implement basic MessageInput functionality

--- client/src/components/MessageInput/MessageInput.js
+++ client/src/components/MessageInput/MessageInput.js
@@ -1,14 +1,35 @@
-import React, { Component } from 'react';
-import './MessageInput.css';
+import React, { Component } from 'react'
+
+import './MessageInput.css'
 
 class MessageInput extends Component {
-  render() {
+  socketClient = window.socketClient
+
+  state = {
+    messageBody: ''
+  }
+
+  handleChange = event => {
+    this.setState({ messageBody: event.target.value })
+  }
+
+  handleSubmit = event => {
+    this.socketClient.emit('message', {
+      sender: 'abc',
+      messageBody: this.state.messageBody
+    })
+
+    event.preventDefault()
+  }
+
+  render = () => {
     return (
-      <div className='message-input-container'>
-        message-input
-      </div>
+      <form className='message-input-container' onSubmit={this.handleSubmit}>
+        <input type='text' value={this.state.messageBody} onChange={this.handleChange} />
+        <input type='submit' value='Send' />
+      </form>
     )
   }
 }
 
-export default MessageInput;
+export default MessageInput
